eddbb13f093f4a4ade7eff4a211f7b35cdb1ee3f
Warren MacEvoy <wmacevoy@gmail.com>
2022-03-10 13:48:41 -0700

    run test

--- js/run-test.ps1
+++ js/run-test.ps1
@@ -11,7 +11,7 @@ if ($buildProc.ExitCode -ne 0) {
 }
 
 $pwd = Get-Location
-$runProc = Start-Process docker.exe  -windowstyle Hidden -ArgumentList "run --rm -v `"$pwd:/app`" -p 8080:8080 js" -PassThru
+$runProc = Start-Process docker.exe  -windowstyle Hidden -ArgumentList "run --rm -v `"${pwd}:/app`" -p 8080:8080 js" -PassThru
 $runProc.WaitForExit();
 
 if ($runProc.ExitCode -ne 0) {
