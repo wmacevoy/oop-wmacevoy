4832dc5351deeffd696aa85fbe2e63756bafaeac
Warren MacEvoy <wmacevoy@gmail.com>
2022-03-10 14:06:17 -0700

    run test

--- js/run-test.ps1
+++ js/run-test.ps1
@@ -1,19 +1,23 @@
-function ExitWithCode($exitcode) {
-  $host.SetShouldExit($exitcode)
+Function ExitWithCode($exitcode) {
+ $host.SetShouldExit($exitcode)
   exit
 }
 
-$buildProc = Start-Process -NoNewWindow docker.exe  -windowstyle Hidden -ArgumentList "build -t js ." -PassThru
-$buildProc.WaitForExit();
-if ($buildProc.ExitCode -ne 0) {
-    Write-Warning "$_ exited with status code $($buildProc.ExitCode)"
-    ExitWithCode($buildProc.ExitCode)
+Function Background() { 
+   $buildProc = Start-Process -NoNewWindow docker.exe  -windowstyle Hidden -ArgumentList "build -t js ." -PassThru
+   $buildProc.WaitForExit();
+   if ($buildProc.ExitCode -ne 0) {
+      Write-Warning "$_ exited with status code $($buildProc.ExitCode)"
+      ExitWithCode($buildProc.ExitCode) 
+   }
+
+   $runProc = Start-Process -NoNewWindow docker.exe  -windowstyle Hidden -ArgumentList "run --rm -v `"$(Get-Location):/app`" -p 8080:8080 js" -PassThru
+   $runProc.WaitForExit();
+
+   if ($runProc.ExitCode -ne 0) {
+      Write-Warning "$_ exited with status code $($runProc.ExitCode)"
+      ExitWithCode($runProc.ExitCode)
+   }
 }
 
-$runProc = Start-Process -NoNewWindow docker.exe  -windowstyle Hidden -ArgumentList "run --rm -v `"$(Get-Location):/app`" -p 8080:8080 js" -PassThru
-$runProc.WaitForExit();
-
-if ($runProc.ExitCode -ne 0) {
-    Write-Warning "$_ exited with status code $($runProc.ExitCode)"
-    ExitWithCode($runProc.ExitCode)
-}
+Start-Process powershell -NoNewWindow {Background}
