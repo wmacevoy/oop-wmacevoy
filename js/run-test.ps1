7e771dbcc39d8bd4d6e6381c00cd79279df1dc43
Warren MacEvoy <wmacevoy@gmail.com>
2022-03-10 13:53:43 -0700

    run test

--- js/run-test.ps1
+++ js/run-test.ps1
@@ -3,15 +3,14 @@ function ExitWithCode($exitcode) {
   exit
 }
 
-$buildProc = Start-Process docker.exe  -windowstyle Hidden -ArgumentList "build -t js ." -PassThru
+$buildProc = Start-Process -NoNewWindow docker.exe  -windowstyle Hidden -ArgumentList "build -t js ." -PassThru
 $buildProc.WaitForExit();
 if ($buildProc.ExitCode -ne 0) {
     Write-Warning "$_ exited with status code $($buildProc.ExitCode)"
     ExitWithCode($buildProc.ExitCode)
 }
 
-$pwd = Get-Location
-$runProc = Start-Process docker.exe  -windowstyle Hidden -ArgumentList "run --rm -v `"${pwd}:/app`" -p 8080:8080 js" -PassThru
+$runProc = Start-Process -NoNewWindow docker.exe  -windowstyle Hidden -ArgumentList "run --rm -v `"$(Get-Location):/app`" -p 8080:8080 js" -PassThru
 $runProc.WaitForExit();
 
 if ($runProc.ExitCode -ne 0) {
