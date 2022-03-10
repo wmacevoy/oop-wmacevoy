6165155cf3ca8ea898fbc0174a066964c0beeeb2
Warren MacEvoy <wmacevoy@gmail.com>
2022-03-10 13:45:07 -0700

    run ps1

---
+++ js/run-test.ps1
@@ -0,0 +1,20 @@
+function ExitWithCode($exitcode) {
+  $host.SetShouldExit($exitcode)
+  exit
+}
+
+$buildProc = Start-Process docker.exe  -windowstyle Hidden -ArgumentList "build -t js ." -PassThru
+$buildProc.WaitForExit();
+if ($buildProc.ExitCode -ne 0) {
+    Write-Warning "$_ exited with status code $($buildProc.ExitCode)"
+    ExitWithCode($buildProc.ExitCode)
+}
+
+$pwd = Get-Location
+$runProc = Start-Process docker.exe  -windowstyle Hidden -ArgumentList "run --rm -v `"$pwd:/app`" -p 8080:8080 js" -PassThru
+$runProc.WaitForExit();
+
+if ($runProc.ExitCode -ne 0) {
+    Write-Warning "$_ exited with status code $($runProc.ExitCode)"
+    ExitWithCode($runProc.ExitCode)
+}
