b38803f234ef2dbf21065182628416b4d0f90eac
Warren MacEvoy <wmacevoy@gmail.com>
2022-03-30 11:57:29 -0600

    stl list and vector

--- cpp/src/test_collections.cpp
+++ cpp/src/test_collections.cpp
@@ -14,18 +14,18 @@
 //         but maybe a lot of code.
 //
 //    2. If Collection is a kind of bag (vector, set, map),
-//         then Collection::itertor is a kind (type) of
+//         then Collection::iterator is a kind (type) of
 //         iterator, which can index into that kind of collection.
 //
 //    3.  These iterators act like pointers.
 //
 //         int a[10];
-//         for (iterator p=a.begin(); p != a.end(); ++p) {
-//             int &ai=*p;
+//         for (const_iterator p=a.cbegin(); p != a.end(); ++p) {
+//             const int &ai=*p;
 //             ai=3;
 //         }
 //
-//         bag = [*begin(),...,*end())
+//         bag = [ *begin(),...,*end() )
 //
 //         for (auto ai : a) { ai = 3; }
 //
@@ -38,22 +38,37 @@ struct Cord {
     int length;
     Cord(int _length) : length(_length) {};
 };
-#include <vector>
+#include <vector> 
+#include <list>
+// O(1) on average insert time.  max insert timne is O(n)
+// inserts/deletes that are not on the end are O(n)
+// random access is O(1)
 #include <algorithm>
 
+typedef std::list <std::string> Strings;
+
+bool CordSort(const Cord &a, const Cord &b) {
+  return a.length < b.length;
+}
+
 TEST(vector,strings) {
-    std::vector < std::string > a(33);
+    Strings  a(33);
     std::vector < Cord > cords(10,Cord(0));
+    // cords[5] = *(cords.begin()+5)
+
+    // a[5]=*(a.begin()+5);
 
 //    Cord cord;
 
     a.push_back("thing");
-    std::sort(a.begin(),a.end());
-    std::vector < std::string > :: iterator i = 
+    a.sort();
+    // std::sort(a.begin(),a.end());
+    Strings::iterator  i = 
       std::find(a.begin(),a.end(), "thing");
 
+    int direction = 1;
     std::sort(cords.begin(),cords.end(),
-      [a](auto x,auto y) { a.size(); return x.length < y.length; });
+       [direction](auto x,auto y) { return direction*x.length < direction*y.length; });
     
     auto j = std::find(a.begin(),a.end(), "thing");
 }
