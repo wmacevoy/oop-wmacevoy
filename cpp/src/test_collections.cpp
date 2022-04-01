0ba96c2d4a1c80de226d17d08ab4784bab8c3749
Warren MacEvoy <wmacevoy@gmail.com>
2022-04-01 11:51:17 -0600

    set/map

--- cpp/src/test_collections.cpp
+++ cpp/src/test_collections.cpp
@@ -40,6 +40,10 @@ struct Cord {
 };
 #include <vector> 
 #include <list>
+//
+// a[3] value is a[key]
+//
+//
 // O(1) on average insert time.  max insert timne is O(n)
 // inserts/deletes that are not on the end are O(n)
 // random access is O(1)
@@ -72,3 +76,67 @@ TEST(vector,strings) {
     
     auto j = std::find(a.begin(),a.end(), "thing");
 }
+
+// map is a mapping from keys (of some type) to values (of some type)
+// vector/list is mapping from 0..n-1 (keys) to values.
+//
+// For maps, you still use m[key]=value stil holds up.
+//
+// Really a map is a set of ordered pairs (key,value), where,
+// the set equality is based on the keys only.
+//
+//   pair<A,B> is the type that maps use.
+//
+// In C++, maps/sets are stored as balanced binary trees.
+//
+//     balanced binary trees are sorted on the key.  The
+//     values don't matter much; they are along for the
+//     ride.
+//
+//     O(log n) insert/delete/find
+//
+//     Upshot: you need to sort keys.
+
+#include <map>
+#include <set>
+#include <stdint.h>
+
+TEST(map,simple) {
+  std::map<uint32_t,double> incomeBySSN;
+  // std::pair<const uint32_t,double>
+
+  incomeBySSN[133'46'7890]=33.33;
+  incomeBySSN[321'33'3333]=12.00;
+
+  for (auto &kv : incomeBySSN) {
+    auto& ssn = kv.first;
+    auto& income = kv.second;
+    std::cout << "ssn" << ssn << " made " << income << std::endl;
+
+    if (ssn == 321'33'3333) { 
+      std::cout << "found" << std::endl; 
+      income = 44.37;
+    }
+
+  }
+
+  for (auto &kv : incomeBySSN) {
+    auto ssn = kv.first;
+    auto income = kv.second;
+    std::cout << "ssn" << ssn << " made " << income << std::endl;
+  }
+
+  std::set<std::string> names;
+
+  names.insert("alice");
+  names.insert("zeta");
+
+  if (names.find("alice") != names.end()) {
+    std::cout << "found alice!";
+  }
+}
+
+// so for your own things, you have to provide some way
+// to order the keys (map) memebers (set).
+
+// solution 1: define a consistent < and ==
